f7643b29059c6aac4d05b94493acd573b7c12db3
Zachary J. Krol <zkrol@thunder.cise.ufl.edu>
2023-05-01 01:32:54 -0400

    Fix syntax error

--- mysql.log.data.php
+++ mysql.log.data.php
@@ -3,3 +3,7 @@ $mysql_login = "";
 $mysql_passw = "";
 $mysql_host = "";
 $mysql_database = "";
+
+// DO NOT COMMIT TO REPO
+// Add to .gitignore 
+// This is just a template. Set these up yourself.
