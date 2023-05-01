1332846b7601a192db0bc2e8f2b7ed10afb6c6c6
Zachary J. Krol <zkrol@thunder.cise.ufl.edu>
2023-05-01 15:38:34 -0400

    Added button routing for each product

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  // Functions go here
+  $(document).on("click", ".product-review-btn", function () {
+    console.log("hello");
+    var snackID = $(this).data("snack-id");
+    window.location.href = "review.php?id=" + snackID;
+  });
+
+  // $(document).on("click", "snack-page-btn", function () {
+  //   var snackID = $(this).data("snack-id");
+  //   window.location.href = ">>>>SNACKPAGEGOESHERE<<<<.php?id=" + snackID;
+  // });
 });
