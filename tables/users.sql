3548d2c6c906232bc86e5a6869b325e628f2b259
Zach Krol <61989585+ZachKrol@users.noreply.github.com>
2023-05-03 03:32:26 -0400

    Hotfix snack table (#32)
    * Update review.php with styling and routing from products page
    
    * Styling fix, vertically center items on large screens
    
    * merge with main
    
    * Fixed register page not working properly
    
    * fixed bug with & in snack names. changed to "and"
    
    * typo fix
    
    ---------
    
    Co-authored-by: Zachary J. Krol <zkrol@thunder.cise.ufl.edu>

--- tables/users.sql
+++ tables/users.sql
@@ -31,14 +31,14 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `userID` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
   `aboutme` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `following` int NOT NULL,
-  `followers` int NOT NULL,
-  `reviews` int NOT NULL, -- PROFILE PIC
+  `following` int NOT NULL DEFAULT 0,
+  `followers` int NOT NULL DEFAULT 0,
+  `reviews` int NOT NULL DEFAULT 0, -- PROFILE PIC
   PRIMARY KEY (`userID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1005 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -47,11 +47,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`userID`, `username`, `password`, `email`, `fname`, `lname`, `aboutme`, `following`, `followers`, `reviews`) VALUES
-(1000, 'test', 'test', 'test@test.com', 'Daniel', 'Dos Santos', 'just testing tings', 233, 221 ,4),
-(1001, 'test2', 'test2', 'test2@test.com', 'test', 'ing', NULL, 0, 0, 0),
-(1002, 'testing1', '$2y$10$7B7A1NWNfm4auRXK4IvEhOBSrZkZbtRJvNzDzrRR279', 'test@test.com', 'Nehemie', 'Yacinthe', NULL, 0, 0, 0),
-(1003, 'testing2', '$2y$10$TWsCnpCZnT4EHMxc.qDmsu43o8xWQIhGuzRM6KCFwVf', 'test@test.com', 'Yacinthe', 'Nehemie', NULL, 0, 0, 0),
-(1004, 'test34', '$2y$10$PlCyJ4GPkTl9UelkkAJWo.9SJeJgbQQp.yUdET8QmJ/', 'test@test.com', 'test', 'test', NULL, 0, 0, 0);
+(1000, 'test', '$2y$10$YRo4B2hJQeYXzq0PaUA7ROQB48Qj3iShDTCrpTPbGD03eOhCIBPuW', 'test@test.com', 'Daniel', 'Dos Santos', 'just testing tings', 233, 221 ,4);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
