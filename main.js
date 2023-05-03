4d6fcdd386cf13e019d904b1b7b8a85a68168f53
Zachary J. Krol <zkrol@thunder.cise.ufl.edu>
2023-05-03 14:41:02 -0400

    took out console.log

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 $(document).ready(function () {
-  $(document).on("click", ".product-review-btn", function () {
-    var snackID = $(this).data("snack-id");
-    window.location.href = "review.php?snackID=" + snackID;
-  });
-
   $(document).on("click", ".snack-page-btn", function () {
-    console.log("clicked");
     var snackID = $(this).data("snack-id");
     window.location.href = "snack.php?snackID=" + snackID;
   });
 
-  // $(document).on("click", ".snack-review-btn", function () {
-  //   console.log("clicked");
-  //   var snackID = $(this).data("snack-id");
-  //   window.location.href = "snack.php?snackID=" + snackID;
-  // });
+  $(document).on("click", ".product-review-btn", function () {
+    var snackID = $(this).data("snack-id");
+    window.location.href = "review.php?snackID=" + snackID;
+  });
 });
