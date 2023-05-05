d8190fb039825bebfdbec4989fc0bca0fcf27bb0
Zachary J. Krol <zkrol@thunder.cise.ufl.edu>
2023-05-04 21:17:14 -0400

    fix ratings

--- products.php
+++ products.php
@@ -38,6 +38,7 @@
 
         //Database connection
         $link = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
+        $link2 = new mysqli($config["servername"], $config["username"], $config["password"], $config["dbname"]);
         // Check connection
         if ($link->connect_error) {
           die("Connection failed: " . $link->connect_error);
@@ -46,13 +47,42 @@
         $sql = "SELECT ID, snackID, pictureURL, name, description, rating, type FROM snacks";
         $result = $link->query($sql);
 
+        //$numReviews = $row2["numReviews"];
+        //$snackID = $row["snackID"];
+        //$rating = $row["rating"];
+        //$pictureURL = $row["pictureURL"];
+        //$snackName = $row["name"];
+        //$type = $row["type"];
+        //$desc = $row["description"];
+
+
         if ($result->num_rows > 0) {
           while ($row = $result->fetch_assoc()) {
+            $sql2 = $link2->prepare("
+            SELECT snacks.snackID, 
+            COUNT(CASE WHEN reviews.snackID IS NOT NULL THEN 1 END) AS numReviews
+            FROM snacks
+            LEFT JOIN reviews ON snacks.snackID = reviews.snackID
+            WHERE snacks.snackID = ?
+            GROUP BY snacks.snackID
+          ");
+
+            $sql2->bind_param("s", $row["snackID"]);
+            $sql2->execute();
+            $result2 = $sql2->get_result();
+            $row2 = $result2->fetch_assoc();
+            $numReviews = $row2["numReviews"];
+            $rating =  $row["rating"];
+
+            if ($numReviews > 0) {
+              $rating =  $rating / $numReviews;
+            }
+            $rating = round($rating, 1);
             echo '
             <div class="flip-card col-xl-3 col-lg-4 col-md-6 p-4">
               <div class="flip-card-inner">
                 <div class="flip-card-front rounded-4">
-                  <p class="h5 mt-3" style="color: #146c43"><span class="small">Rating: </span>' . $row["rating"] . '</p>
+                  <p class="h5 mt-3" style="color: #146c43"><span class="small">Rating: </span>' . $rating . '</p>
                   <div class="d-flex align-items-center justify-content-center" style="height: 250px; width: 100%">
                     <img class="m-2" src="./images/' . $row["pictureURL"] . '" alt="' . $row["name"] . '" style="max-height: 90%; max-width: 90%;" />
                   </div>
@@ -60,7 +90,7 @@
                   <p class="h6 text-secondary mx-2 pt-3 pt-md-0">' . $row["type"] . '</p>
                 </div>
                 <div class="flip-card-back rounded-4">
-                  <p class="h5 mt-3" style="color: #a6e9d5"><span class="small">Rating: </span>' . $row["rating"] . '</p>
+                  <p class="h5 mt-3" style="color: #a6e9d5"><span class="small">Rating: </span>' . $rating . '</p>
                   <p class="lg-small px-1 m-2 overflow-auto" style="height:210px">' . $row["description"] . '</p>
                   <div class="py-2">
                     <button type="button" class="btn btn-light mx-2 snack-page-btn" data-snack-id="' . $row["snackID"] . '" style="min-width: 90px">More Info</button>
